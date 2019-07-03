1e98d842e9aa6e90508d488494d5a5e1c62a279e
Oca_oca <luisdonaldogarciacastro@gmail.com>
2019-07-03 11:12:24 -0500

    avance en clase

--- components/portfolio/showProject.php
+++ components/portfolio/showProject.php
@@ -9,16 +9,16 @@
         ?>
         <div class="projectSelector">
             <div class="projectSelector__title">
-
+        
             </div>
             <div class="projectSelector__container">
                 <div class="card">
                     <div class="card-img">
-                            <img src="<?php echo $card[$i]->get_img() ?>" alt="None" class="card-img__img"/>
+                        <img src="<?php echo $card[$i]->get_img() ?>" alt="None" class="card-img__img"/>
                     </div>
                     <div class="card-info">
-                            <h1 class="card-title"><?php echo $card[$i]->get_title() ?></h1>
-                            <h3><?php echo $card[$i]->get_description() ?></h3>
+                        <h1 class="card-title"><?php echo $card[$i]->get_title() ?></h1>
+                        <h3><?php echo $card[$i]->get_description() ?></h3>
                     </div>
                     
                 </div>
